d4fc4e4a9a2b4e00f065736fdfc9e654c54423b5
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-12 08:44:57 -0800

    custom hook completed

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+const useForm = (initialValues) => {
+    const [formValues, setFormValues] = useState(initialValues)
+
+    const handleChanges= (e) =>{
+        setFormValues({...formValues, [e.target.name]:e.target.value})
+    }
+
+    return[formValues, handleChanges] 
+}
+export default useForm;
